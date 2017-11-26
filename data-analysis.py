59d02f694f37ef821d0cf5bf635ae9bf69e6770a
Oberyn18 <carlosab1802@gmail.com>
2017-11-26 12:07:57 -0200

    Finish data preprocess.

--- data-analysis.py
+++ data-analysis.py
@@ -3,4 +3,11 @@ import pandas as pd
 
 # Import data using pandas
 data = pd.read_csv("vgsales.csv")
+#data.info()
+
+# Drop NaN values
+data.dropna(how="any", inplace=True)
+
+# Change data type on Year attribute
+data.Year = data.Year.astype(int)
 data.info()
